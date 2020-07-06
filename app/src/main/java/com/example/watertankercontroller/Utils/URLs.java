260c487e9bab18fff230694cd1eb00d3c0992644
kookyinfomedia <kookyinfomedia@gmail.com>
2020-07-06 13:33:20 +0530

    Map Updated

--- app/src/main/java/com/example/watertankercontroller/Utils/URLs.java
+++ app/src/main/java/com/example/watertankercontroller/Utils/URLs.java
@@ -1,7 +1,7 @@
 package com.example.watertankercontroller.Utils;
 
 public class URLs {
-    public static final String BASE_URL = "http://3.6.67.124:8081/api/controller/";
+    public static final String BASE_URL = " http://ec2-18-190-47-17.us-east-2.compute.amazonaws.com:8081/api/controller/";
     public static final String SIGN_IN_URL = "signin";
     public static final String SIGN_OUT_URL = "signout";
     public static final String CREATE_BOOKING = "booking";
